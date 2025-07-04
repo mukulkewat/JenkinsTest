64cb9e5f1f6678f591c54c2cd7b53a31b6a7ac40
Mukul17 <162608441+mukulkewat@users.noreply.github.com>
2025-07-04 12:34:26 +0530

    Delete src directory

--- src/com/ngs/WebContent/Demo.js
+++
@@ -1,7 +0,0 @@
-require(["dojo/_base/declare",
-         "dojo/_base/lang"], 
-function(declare, lang) {		
-	/**
-	 * Use this function to add any global JavaScript methods your plug-in requires.
-	 */
-});
